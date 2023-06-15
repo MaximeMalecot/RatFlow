e22ca6238acf99d88ae2f3f71df9e412f97489bf
julian-saleix <julian.saleix@gmail.com>
2023-06-15 15:25:58 +0200

    Can add / remove origins from front app

--- front/src/components/settings/base-settings.tsx
+++ front/src/components/settings/base-settings.tsx
@@ -7,12 +7,47 @@ import { displayMsg, notify } from "../../utils/toast";
 export default function BaseSettings() {
     const { app } = useAppContext();
     const [loading, setLoading] = useState(false);
+    const [origins, setOrigins] = useState<string[]>(app.origins);
+    const [typedOrigin, setTypedOrigin] = useState<string>("");
 
     const {
         register: registerField,
         handleSubmit,
         formState: { errors },
     } = useForm();
+
+    const removeOrigin = (origin: string) => {
+        setOrigins((prev) => prev.filter((o) => o !== origin));
+    };
+
+    const addOrigin = () => {
+        const origin = typedOrigin.trim();
+        try {
+            if (origin.length == 0 || origin.trim().length == 0) {
+                throw new Error("Origin cannot be empty");
+            }
+            if (origins.includes(origin)) {
+                throw new Error("Origin already exists");
+            }
+
+            setOrigins((prev) => [...prev, origin]);
+            setTypedOrigin("");
+        } catch (e: any) {
+            displayMsg(e.message, "error");
+        }
+    };
+
+    const updateOrigins = async () => {
+        try {
+            setLoading(true);
+            await appService.updateAppOrigins(app._id, origins);
+            notify("Origins updated");
+        } catch (e: any) {
+            displayMsg(e.message, "error");
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const updateAppName = async (data: any) => {
         try {
@@ -63,22 +98,65 @@ export default function BaseSettings() {
                     )}
                 </form>
                 <div className="divider my-0" />
-                <div>
+                <div className="flex flex-col gap-3">
                     <label className="label">
                         <span className="label-text">Origin(s) allowed</span>
                     </label>
-                    {app.origins.length == 0 ? (
+                    {origins.length == 0 ? (
                         <p>There is no origin specified for your app</p>
                     ) : (
-                        app.origins.map((origin, key) => (
-                            <div
-                                key={key}
-                                className="badge badge-secondary badge-outline"
-                            >
-                                {origin}
-                            </div>
-                        ))
+                        <div className="flex gap-5">
+                            {origins.map((origin, key) => (
+                                <div
+                                    key={key}
+                                    onClick={() => removeOrigin(origin)}
+                                    className="badge badge-secondary badge-outline cursor-pointer"
+                                >
+                                    {origin}
+                                </div>
+                            ))}
+                        </div>
                     )}
+                    <div className="flex flex-col gap-5">
+                        <div className="flex items-center gap-3">
+                            <input
+                                type="text"
+                                className={
+                                    "input input-bordered w-4/5 !outline-none outline-0 !border-1 !border-slate-300 "
+                                }
+                                placeholder="Add origin"
+                                value={typedOrigin}
+                                onChange={(e) =>
+                                    setTypedOrigin(e.currentTarget.value)
+                                }
+                                onKeyDown={(e) => {
+                                    if (e.key == "Enter") {
+                                        addOrigin();
+                                        e.currentTarget.value = "";
+                                    }
+                                }}
+                            />
+                            <button
+                                onClick={addOrigin}
+                                className="btn btn-primary w-1/5"
+                            >
+                                Add
+                            </button>
+                        </div>
+
+                        {!loading ? (
+                            <button
+                                onClick={updateOrigins}
+                                className="btn btn-primary w-full"
+                            >
+                                Apply new origins
+                            </button>
+                        ) : (
+                            <button disabled className="btn btn-primary w-full">
+                                Processing
+                            </button>
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
